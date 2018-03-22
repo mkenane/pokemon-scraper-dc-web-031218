e5cf1a959c38423a8a7207c40fd99b6d0dae16cd
mkenane <morkenan@hotmail.com>
2018-03-22 16:49:07 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 
 
 # Scraping
-gem 'nokogiri'
+gem 'nokogiri', '= 1.8.2'
 
 # Database
 gem 'sqlite3'
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
+require 'pry'
+
 class Pokemon
+
+attr_accessor :type, :db, :id, :name
+
+def initialize(name:, type:, db:, id:)
+  @name = name
+  @type = type
+  @db = db
+  @id = id
+end
+
+def self.save(name, type, db)
+  db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  db.last_insert_row_id
+end
+
+def self.find(id, db)
+  poke = db.execute("SELECT * FROM pokemon WHERE id = (?)", id)
+
+  Pokemon.new(id: poke[0][0], name: poke[0][1], type: poke[0][2], db: db)
+end
+
+# def self.save(name, breed, age, database_connection)
+#     database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+#   end
+# end
+
 end
